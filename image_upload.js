d7b0c947a466d6602cdf44bf5cac29e9fae6ef36
braintimes16 <75879886+braintimes16@users.noreply.github.com>
2021-09-19 21:58:07 +0900

    Add files via upload

--- image_upload.js
+++ image_upload.js
@@ -39,45 +39,45 @@ async function predict() {
     //추가하고 싶으면 여기 바꿔~~^^
     switch (prediction[0].className) {
         case "Prada":
-            resultTitle = "절제된 고급스러움, <br> 인간 프라다";
+            resultTitle = "절제된 고급스러움, 인간 프라다";
             resultExplain =
                 "심플함 속에 고급스러움이 느껴지는 당신. 미니멀한 디자인에 프라다 로고만으로 존재감을 압도하는 이탈리아 브랜드 프라다를 닮았네요.프라다를 입은 악마보다 더 프라다가 잘 어울리네요.";
             resultCeleb = "프라다 앰버서더 : 레드벨벳 아이린, EXO 찬열";
             break;
         case "Chanel":
-            resultTitle = "클래식한 트렌디함, <br> 인간 샤넬";
+            resultTitle = "클래식한 트렌디함, 인간 샤넬";
             resultExplain =
                 "자유로우면서도 클래식한, 트렌디하면서도 우아한 매력의 당신. 1910년 코코 샤넬이 설립한 프랑스의 하이엔드 샤넬과 잘어울리네요. 시크한 당신의 매력을 표현해봐요";
             resultCeleb = "샤넬 앰버서더 : 블랙핑크 제니, 김고은, 이성경";
             break;
         case "Diol":
-            resultTitle = "단아하지만 강인한 매력, <br> 인간 디올";
+            resultTitle = "단아하지만 강인한 매력, 인간 디올";
             resultExplain =
                 " 직선보다 곡선이, 시크함보다는 우아함이 잘어울리는 당신. 1947년 동명의 프랑스 디자이너가 파리에서 만든 럭셔리 브랜드 디올과 잘어울리네요. 청순하고 단아한 당신의 매력을 표현해봐요 ";
             resultCeleb = "디올 앰버서더: 수지, 블랙핑크 지수";
             // containerColor = "pink";
             break;
         case "Gucci":
-            resultTitle = "톡톡 튀는 매력, <br> 인간 구찌";
+            resultTitle = "톡톡 튀는 매력, 인간 구찌";
             resultExplain =
                 "힙한 매력의 소유자인 당신. 1921년 구찌오 구찌가 설립한 럭셔리 이탈리아 브랜드 구찌가 잘어울리네요. 톡톡 튀는 당신만의 개성을 표현해봐요";
             resultCeleb = "구찌 앰버서더: 아이유, 엑소 카이";
             break;
         case "YSL":
-            resultTitle = "모던 클래식, <br> 인간 생로랑";
+            resultTitle = "모던 클래식, 인간 생로랑";
             resultExplain =
                 "모던하면서도 시크한 분위기의 소유자인 당신, 1961년 프랑스 파리에서 설립한 브랜드 생로랑과 잘어울리네요. 당신만의 모던 시크한 매력을 뽐내봐요";
             resultCeleb = "생로랑 앰버서더: 블랙핑크 로제";
             break;
         case "Louis_vuitton":
-            resultTitle = "당당함에서 나온 고급스러움, <br> 인간 루이비통";
+            resultTitle = "당당함에서 나온 고급스러움, 인간 루이비통";
             resultExplain =
                 "나다움이 제일 중요한 당신, 1854년에 세워진 프랑스의 하이엔드급 명품 브랜드 루이비통과 잘어울리네요. 깔끔하면서도 고급스러운 당신의 매력을 표현해봐요";
             resultCeleb = "루이비통 앰버서더: 배두나, 방탄소년단";
             break;
         case "Fendi":
             resultTitle =
-                "부드러움과 강인함, 자신감 넘치는 애티튜드. <br> 인간 펜디";
+                "부드러움과 강인함, 자신감 넘치는 애티튜드. 인간 펜디";
             resultExplain =
                 "조곤조곤 할말 다할 것 같은 부잣집 사모님 같은 매력의 당신, 1925년에 설립된 이탈리아 하이앤드 명품 브랜드 펜디와 잘어울리네요.  자신감 넘치는 태도에서 나온 고급스러움을 표현해봐요";
             resultCeleb = "펜디 앰버서더 : 송혜교";
